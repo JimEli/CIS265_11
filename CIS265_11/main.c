8575a697ee765d5baf61655953c00bbc3906959b
Jim Eli <31859133+JimEli@users.noreply.github.com>
2022-03-27 21:29:54 -0600

    Update main.c

--- CIS265_11/main.c
+++ CIS265_11/main.c
@@ -50,19 +50,18 @@
 
 #define MAX_DIGITS 10 // Maximum digits for entry/display.
 
-// Holds characters for 7-segment display of entered digits.
+// Holds characters for 7-segment display of the entered digits.
 char digits[3][(MAX_DIGITS * 4) + 1] = { 0 };
 
 // Store 7-segment characterization of a digit (0-9).
 void process_digit(int digit, int position) {
-	/*
-	   Defines which 7-segment(s) of digit are displayed (0==off, 1==on). 
-	   Encoded as (MSB to LSB): (1) top-left segment, (2) top horizontal, (3) top-right, (4) space, 
-	   (5) left-middle vertical, (6) middle horizontal, (7) right-middle vertical, (8) space, 
-	   (9) left-bottom vertical, (10) bottom horizontal, (11) right-bottom vertical, (12) space.
-	   The 7-segments are defined by the x bits: 0b0x00xxx0xxx0.
-	*/
-	unsigned short int segments[10] = {
+	// Defines which 7-segment(s) of digit are displayed (0==off, 1==on). 
+	// Encoded as (MSB to LSB): (1) top-left segment, (2) top horizontal, (3) top-right, (4) space, 
+	// (5) left-middle vertical, (6) middle horizontal, (7) right-middle vertical, (8) space, 
+	// (9) left-bottom vertical, (10) bottom horizontal, (11) right-bottom vertical, (12) space.
+	// The 7-segments are defined by the x bits: 0b0x00xxx0xxx0.
+	unsigned short int segments[10] = 
+	{
 		0b0100'1010'1110, // 0
 		0b0000'0010'0010, // 1
 		0b0100'0110'1100, // 2
@@ -80,7 +79,8 @@ void process_digit(int digit, int position) {
 	unsigned short int bit = 0b1000'0000'0000;
 
 	// Store segment characters into digits array.
-	for (int i = 0; i < 4; i++, bit >>= 1) {
+	for (int i = 0; i < 4; i++, bit >>= 1) 
+	{
 		digits[0][position * 4 + i] = (segments[digit] & bit) ? segmentCharacters[i] : ' ';      // Top row.
 		digits[1][position * 4 + i] = (segments[digit] & bit >> 4) ? segmentCharacters[i] : ' '; // Middle row.
 		digits[2][position * 4 + i] = (segments[digit] & bit >> 8) ? segmentCharacters[i] : ' '; // Bottom row.
@@ -88,39 +88,46 @@ void process_digit(int digit, int position) {
 }
 
 // Completely clear digit array.
-void clear_digits_array(void) {
+void clear_digits_array(void) 
+{
 	memset(digits, 0, sizeof(digits[0][0]) * sizeof(char) * ((MAX_DIGITS * 4) + 1) * 3);
 }
 
 // Iterate digit array printing it.
-void print_digits_array(void) {
+void print_digits_array(void) 
+{
 	for (int i = 0; i < 3; fprintf(stdout, "%s\n", digits[i++])); 
 	fputs("\n", stdout);
 }
 
 // Program starts here.
-int main(void) {
+int main(void) 
+{
 	char number[MAX_DIGITS + 2]; // Holds user entered numbers (LF and terminator).
 
 	// Enter number.
 	fputs("Enter an integer number (up to 10 digits): ", stdout);
-	if (!fgets(number, sizeof number, stdin)) {
+	if (!fgets(number, sizeof number, stdin)) 
+	{
 		fputs("Fatal program error!\n", stderr);
 		exit(EXIT_FAILURE);
 	}
-	else if (!strchr(number, '\n')) {
+	else if (!strchr(number, '\n')) 
+	{
 		// Input too long, eat remainder.
 		while (fgets(number, sizeof number, stdin) && !strchr(number, '\n'))
 			; // Empty body.
 		fputs("Too many characters input.\n", stdout);
 	}
-	else {
+	else 
+	{
 		unsigned int i = 0, j = 0; // Separate array indicies.
 
 		clear_digits_array();
 
 		// Iterate through input, checking for digits and process accordingly.
-		while (i < strlen(number) && j < MAX_DIGITS) {
+		while (i < strlen(number) && j < MAX_DIGITS) 
+		{
 			if (isdigit(number[i])) 
 				process_digit(number[i] - '0', j++);
 			i++;
